bc96f3941b6f2c2426af56187e2d3d2e3d5d31b4
ainyeen <ainali1912@gmail.com>
2025-05-25 16:18:14 +0800

    Delete Task1 directory

--- Task1/app.js
+++
@@ -1,8 +0,0 @@
-// Register service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('serviceworker.js')
-      .then(reg => console.log('Service Worker registered', reg))
-      .catch(err => console.log('Service Worker registration failed', err));
-  });
-}
